80dd64c7d775d55eb480c7834bc42704e56a972f
Umid-R <umidraxmatullayev96@gmail.com>
2026-01-19 20:07:50 +0900

    update the game

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from PIL import Image
+import pyautogui
 
 
 
@@ -15,10 +17,37 @@ driver.get("https://elgoog.im/t-rex/")
 
 
 # Wait until the page is loaded 
+time.sleep(1)
+
+random_element=driver.find_element(By.TAG_NAME,"body")
+
+
+# Start the game 
+time.sleep(1)
+random_element.send_keys(Keys.SPACE)
+
 time.sleep(3)
 
-random_element=driver.find_element(by=By.NAME,'div')
 
 
+while True:
+    screenshot = pyautogui.screenshot()
+    region = screenshot.crop((250, 1200,600, 1400))
+    gray = region.convert("L")
+    pixels = gray.load()
 
+    obstacle = False
+    for x in range(gray.width):
+        for y in range(gray.height):
+            if pixels[x, y] < 150:
+                obstacle = True
+                break
+        if obstacle:
+            break
+
+    if obstacle:
+        
+        random_element.send_keys(Keys.UP)
+
+    time.sleep(0.01)
 
